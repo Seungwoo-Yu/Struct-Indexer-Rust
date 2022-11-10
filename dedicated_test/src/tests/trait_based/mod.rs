8787b1d4c919280963a1adbafe4cd84dd0de1506
Seungwoo-Yu <seungwooyu2000@gmail.com>
2022-11-11 02:28:24 +0900

    dedicated_test: fix trait_based tests
    fix test1 for changes from core
    add test2 for rc and arc container support

--- dedicated_test/src/tests/trait_based/mod.rs
+++ dedicated_test/src/tests/trait_based/mod.rs
@@ -1,5 +1,7 @@
 use std::ops::Deref;
-use struct_indexer_core::{InstanceOf, SameTypeWith, ToNamedStruct};
+use std::rc::Rc;
+use std::sync::Arc;
+use struct_indexer_core::{InstanceOf, SameTypeWith, ToNamedArcStruct, ToNamedBoxStruct, ToNamedRcStruct};
 use crate::tests::trait_based::models::a::A;
 use crate::tests::trait_based::models::b::B;
 use crate::tests::trait_based::models::base_trait::BaseTrait;
@@ -18,11 +20,11 @@ fn test1() {
     // Therefore, Box and data inside Box cannot be copied.
 
     // Get B instance
-    let b_box = container.pop().unwrap().to_named_struct::<B>().unwrap();
+    let b_box = container.pop().unwrap().to_named_box_struct::<B>().unwrap();
     let b = b_box.deref();
 
     // Get A instance
-    let a_box = container.pop().unwrap().to_named_struct::<A>().unwrap();
+    let a_box = container.pop().unwrap().to_named_box_struct::<A>().unwrap();
     let a = a_box.deref();
 
     println!("{}", a.hello_world());
@@ -30,4 +32,43 @@ fn test1() {
 
     assert_eq!(a.same_type_with(b).unwrap(), false);
     assert_eq!(b.instance_of::<A>().unwrap(), false);
+}
+
+#[test]
+fn test2() {
+    let rc_vec: Vec<Rc<dyn BaseTrait>> = vec![Rc::new(A {}), Rc::new(B {})];
+
+    // Unlike Box, reference of Rc can be cloned!
+
+    let rc_ref_a = rc_vec.get(0).unwrap();
+    // Clone new reference for A and change its type to named struct
+    let rc_cloned_a = Rc::clone(rc_ref_a).to_named_rc_struct::<A>().unwrap();
+
+    let rc_ref_b = rc_vec.get(1).unwrap();
+    // Clone new reference for B and change its type to named struct
+    let rc_cloned_b = Rc::clone(rc_ref_b).to_named_rc_struct::<B>().unwrap();
+
+    println!("{}", rc_cloned_a.hello_world());
+    println!("{}", rc_cloned_b.hello_world());
+
+    assert_eq!(rc_cloned_a.instance_of::<B>().unwrap(), false);
+    assert_eq!(rc_cloned_b.same_type_with(rc_cloned_b.deref()).unwrap(), true);
+
+    let rc_vec: Vec<Arc<dyn BaseTrait>> = vec![Arc::new(A {}), Arc::new(B {})];
+
+    // Unlike Box, reference of Arc can be cloned!
+
+    let rc_ref_a = rc_vec.get(0).unwrap();
+    // Clone new reference for A and change its type to named struct
+    let rc_cloned_a = Arc::clone(rc_ref_a).to_named_arc_struct::<A>().unwrap();
+
+    let rc_ref_b = rc_vec.get(1).unwrap();
+    // Clone new reference for B and change its type to named struct
+    let rc_cloned_b = Arc::clone(rc_ref_b).to_named_arc_struct::<B>().unwrap();
+
+    println!("{}", rc_cloned_a.hello_world());
+    println!("{}", rc_cloned_b.hello_world());
+
+    assert_eq!(rc_cloned_a.instance_of::<B>().unwrap(), false);
+    assert_eq!(rc_cloned_b.same_type_with(rc_cloned_b.deref()).unwrap(), true);
 }
